f5200d688f99b7b6446876451f501dadd4e80ef8
Arpan <arpanmahanty01@gmail.com>
2023-03-13 19:12:33 +0530

    adds google oAuth

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const {v4:uuid} = require('uuid');
 const bodyParser = require('body-parser');
 const User = require('./models/user');
+const axios = require('axios');
+const jwt = require ('jsonwebtoken');
+
 app.use(express.urlencoded({extended:true}));
 const mongoose = require('mongoose');
 mongoose.set('strictQuery',false);
@@ -91,6 +94,78 @@ app.post('/peerJs',async(req,res)=>{
     
 })
 
+function getGoogleAuthURL(){
+    const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+    const options = {
+        redirect_uri:'http://localhost:8080/TeleCode',
+        client_id:'533826739878-4kao3g422kdc72rhqkfft6lbausu78lp.apps.googleusercontent.com',
+        access_type:'offline',
+        response_type:'code',
+        prompt:'consent',
+        scope:[
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ].join(' '),
+    };
+    const qs = new URLSearchParams(options);
+
+    return `${rootUrl}?${qs.toString(options)}`
+}
+
+app.get("/TeleCode/SignIn",(req,res)=>{
+    res.redirect(getGoogleAuthURL())
+});
+
+
+function getTokens(code,clientId,clientSecret,redirectUri){
+const url = 'https://oauth2.googleapis.com/token';
+const values = {
+    code,
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uri: redirectUri,
+    grant_type: "authorization_code",
+};
+    const qs = new URLSearchParams(values);
+return axios.post(url,qs.toString(values),{
+    headers:{
+        'Content-Type':'application/x-www-form-urlencoded',
+    },
+})
+.then((res)=>res.data)
+.catch((error)=>{
+    console.error('failed to fetch auth tokens');
+});
+}
+    
+
+
+app.get('/api/sessions/oauth/google',async(req,res)=>{
+    const code = req.query.code;
+
+    const {id_token,access_token} = await getTokens({
+        code,
+        clientId:'533826739878-4kao3g422kdc72rhqkfft6lbausu78lp.apps.googleusercontent.com',
+        clientSecret:'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',
+        redirectUri:'http://localhost:8080/TeleCode'
+    });
+
+    const  googleUser = await axios.get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
+        {
+            headers: {
+              Authorization: `Bearer ${id_token}`,
+            },
+          }
+    )
+    .then((res)=> res.data)
+    .catch((error)=>{
+        console.error('Failed to fetch user');
+        throw new Error(error.message);
+    });
+
+
+})
 
 app.listen(8080,()=>{
     console.log('SERVER RUNNING')
